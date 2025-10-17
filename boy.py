67be10bfa4967df7b4947fcc3a94edb69065320a
OhSieun <sieun125@tukorea.ac.kr>
2025-10-17 18:52:46 +0900

    AutoRun 상태 좌우측 끝 도달하면 자동 방향전환 조건문 추가

--- boy.py
+++ boy.py
@@ -35,6 +35,9 @@ class AutoRun:
     def do(self):
         self.boy.frame = (self.boy.frame + 1) % 8
         self.boy.x += self.boy.dir * 10
+        if self.boy.x < 0 or self.boy.x > 800:
+            self.boy.dir *= -1
+            self.boy.face_dir *= -1
 
     def draw(self):
         if self.boy.face_dir == 1: # right
